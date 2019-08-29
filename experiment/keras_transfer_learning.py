9a157d21be2b780f0ae9cf897c71c909f3904ef6
Henry Z Chen <henry.z.chen@pwc.com>
2019-08-29 17:02:54 +0800

    add data generator

--- experiment/keras_transfer_learning.py
+++ experiment/keras_transfer_learning.py
@@ -30,4 +30,36 @@ x = Dense(1024, activation='relu')(x)
 pretiction = Dense(16, activation='softmax')(x)
 model_final = Model(inputs=model.input, outputs=pretiction)
 model_final.compile(loss='categorical_crossentropy', optimizer=optimizers.SGD(lr=0.0001, momentum=0.9), metrics=['accuracy'])
-
+train_datagen = ImageDataGenerator(
+	rescale=1./255,
+	horizontal_flip=True,
+	fill_mode='nearest',
+	zoom_range=0.3,
+	width_shift_range=0.3,
+	height_shift_range=0.3,
+	rotation_range=30
+)
+test_datagen = ImageDataGenerator(
+	rescale=1./255,
+	horizontal_flip=True,
+	fill_mode='nearest',
+	zoom_range=0.3,
+	width_shift_range=0.3,
+	height_shift_range=0.3,
+	rotation_range=30
+)
+train_generator = train_datagen.flow_from_directory(
+	train_data_dir,
+	target_size=(img_height, img_width),
+	batch_size=batch_size,
+	class_mode='categorical'
+)
+validation_generator = test_datagen.flow_from_directory(
+	validation_dir,
+	target_size=(img_height, img_width),
+	batch_size=batch_size,
+	class_mode='categorical'
+)
+checkpoint = ModelCheckpoint('vgg16_1.h5', monitor='val_acc', verbose=1, save_best_only=True, save_weights_only=True, mode='auto', period=1)
+early = EarlyStopping(monitor='val_acc', min_delta=0, patience=10, verbose=1, mode='auto')
+model_final.fit_generator(train_generator, steps_per_epoch=nb_train_samples, epochs=epochs, validation_data=validation_generator, validation_steps = nb_validation_samples, callbacks=[checkpoint, early])
